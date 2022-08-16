5beba68c494cb74976feb6703f2d4c3d53e92baf
arynthernium <arynthernium@gmail.com>
2022-08-16 12:33:09 -0500

    add some desktop background settings

--- js/boot.js
+++ js/boot.js
@@ -24,4 +24,53 @@ window.onload = () => {
 	window.SYSTEM.settings.listen('acrylicEnabled', function listenForAcrylicEnabledChange(value) {
 		value ? document.body.classList.add('acrylicEnabled') : document.body.classList.remove('acrylicEnabled');
 	});
+
+	window.SYSTEM.settings.listen('desktopBackgroundImage', function listenForDesktopBackgroundImageChange(value) {
+		try {
+			const url = new URL(value);
+			document.body.style.setProperty('--wallpaper', 'url("' + url.toString() + '")');
+		} catch (err) {
+			if (err instanceof TypeError) {
+				document.body.style.setProperty('--wallpaper', 'url("' + '../images/wp' + (window.SYSTEM.settings.darkMode ? 'dark' : 'light') + '.jpg' + '")');
+			} else throw err;
+		};
+	});
+	window.SYSTEM.settings.desktopBackgroundImage = window.SYSTEM.settings.desktopBackgroundImage || 'url("' + '../images/wp' + window.SYSTEM.settings.darkMode + '.jpg' + '")';
+	window.SYSTEM.settings.listen('desktopBackgroundFit', function listenForDesktopBackgroundFitChange(value) {
+		switch (value) {
+			case 'auto':
+				document.body.style.backgroundSize = 'auto';
+				break;
+			case 'cover':
+				document.body.style.backgroundSize = 'cover';
+				break;
+			case 'contain':
+				document.body.style.backgroundSize = 'contain';
+				break;
+			default:
+				window.SYSTEM.settings.desktopBackgroundFit = 'cover';
+				break;
+		};
+	});
+	window.SYSTEM.settings.desktopBackgroundFit = window.SYSTEM.settings.desktopBackgroundFit;
+	window.SYSTEM.settings.listen('desktopBackgroundRepeat', function listenForDesktopBackgroundRepeatChange(value) {
+		switch (value) {
+			case 'repeat':
+				document.body.style.backgroundRepeat = 'repeat';
+				break;
+			case 'no-repeat':
+				document.body.style.backgroundRepeat = 'no-repeat';
+				break;
+			case 'space':
+				document.body.style.backgroundRepeat = 'space';
+				break;
+			case 'round':
+				document.body.style.backgroundRepeat = 'round';
+				break;
+			default:
+				window.SYSTEM.settings.desktopBackgroundRepeat = 'no-repeat';
+				break;
+		};
+	});
+	window.SYSTEM.settings.desktopBackgroundRepeat = window.SYSTEM.settings.desktopBackgroundRepeat;
 };
